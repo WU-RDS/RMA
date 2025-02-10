4ad794471175467f92a560e5c8c2c41710c38bdd
Daria Yudaeva <ddyudaeva@gmail.com>
2025-02-10 12:36:49 +0100

    upd data

--- Code/01-basic_data_handling.R
+++ Code/01-basic_data_handling.R
@@ -1,18 +1,16 @@
-## --------------------------------------------------------------------------------------------------------------------------------------------------------------
+## This is the beginning of the code
+
+## Remember to run the code line by line, without skipping any of lines, top-to-bottom (i.e., never start in the middle of the code)
+
 x <- "hello world" # assigns the words "hello world" to the object x
-# this is a comment
+# this is a comment (any number of "#" denotes a comment; the number of "#" does not matter)
 
-
-## --------------------------------------------------------------------------------------------------------------------------------------------------------------
 x # print the value of x to the console
-
-
-## --------------------------------------------------------------------------------------------------------------------------------------------------------------
-print(x) # print the value of x to the console
+#print(x) # - another way to print the results (in this case, the value of x)
 
 ## --------------------------------------------------------------------------------------------------------------------------------------------------------------
 x <- 2 # assigns the value of 2 to the object x
-print(x)
+x
 x == 2  # checks whether the value of x is equal to 2
 x != 3  # checks whether the value of x is NOT equal to 3
 x < 3   # checks whether the value of x is less than 3
@@ -26,78 +24,55 @@ x*y # multiplication of x and y
 x + y # adds the values of x and y together
 y^2 + 3*x # adds the value of y squared and 3x the value of x together
 
+# such operations can be performed not just with "single" values; we can do the same with values in data tables (see further)
 
 ## --------------------------------------------------------------------------------------------------------------------------------------------------------------
-class(y)
+#class(y) # check the class of the variable (what type of variable it is) - or hover over the "y" cell in the top right corner (Global Environment)
 y <- as.character(y)
 y <- as.numeric(y)
-print(y)
+y
 
 
 ## --------------------------------------------------------------------------------------------------------------------------------------------------------------
+## Data Import
+
+### Manual:
 # Numeric:
-top10_sales <- c(163608, 126687, 120480, 110022, 108630, 95639, 94690, 89011, 87869, 85599) # function c() means "concatenate" (= merge) and serves for creating a row of values
-log(top10_sales) # you can take logarithm of the values in the row you created by applying the function log() to the whole object that contains the values of interest. It also works with columns
+sales <- c(163608, 126687, 120480, 110022, 108630, 95639, 94690, 89011, 87869, 85599) # function c() means "concatenate" (= merge) and serves for creating a row of values
+log(top10_sales) # you can take logarithm of the values in the row you created by applying the function log() to the whole object that contains the values of interest. 
+# It also works with columns in data tables (see further)
+
 # Character:
-top10_product_names <- c("Bio-Kaisersemmel", "Laktosefreie Bio-Vollmilch", "Ottakringer Helles", "Milka Ganze Haselnüsse", "Bio-Toastkäse Scheiben", "Ottakringer Bio Zwickl", "Vienna Coffee House Espresso", "Bio-Mozzarella", "Basmati Reis", "Bananen") # Characters have to be put in ""
+products <- c("Bio-Kaisersemmel", "Laktosefreie Bio-Vollmilch", "Ottakringer Helles", "Milka Ganze Haselnüsse", "Bio-Toastkäse Scheiben", "Ottakringer Bio Zwickl", "Vienna Coffee House Espresso", "Bio-Mozzarella", "Basmati Reis", "Bananen") # Characters have to be put in ""
 
-# Factor variable with two categories:
-private_label <- c(1,1,0,0,1,0,0,1,1,1)
-private_label <- factor(private_label, 
-                               levels = 0:1, 
-                               labels = c("national brand", "private label"))
 
-# Factor variable with more than two categories:
-top10_brand <- c("Ja! Natürlich", "Ja! Natürlich", "Ottakringer", "Milka", "Ja! Natürlich", "Ottakringer", "Julius Meinl", "Ja! Natürlich", "Billa Bio", "Clever")
-top10_brand <- as.factor(top10_brand)
+### Data files: 
+## in this course, we use data files, stored on a server; these files can be read into R as follows
+sales_data <- read.csv("https://raw.githubusercontent.com/WU-RDS/RMA2024/main/data/Sales_Data,csv", 
+                      sep = ";", 
+                      header = TRUE)
 
-# Date:
-date_most_sold <- as.Date(c("2023-05-24", "2023-06-23", "2023-09-01", "2023-06-30", "2023-05-05", "2023-06-09", "2023-07-14", "2023-06-16", "2023-05-18", "2023-05-19"))
-
-# Logical
-private_label_logical <- c(TRUE,TRUE,FALSE,FALSE,TRUE,FALSE,FALSE,TRUE,TRUE,TRUE)  
+# If you ever need to read in files from your local device (your laptop, etc.), use the code below. 
+# You must check where the file is saved (provide the full file path), how it is named, and (in case of csv. files) what separators it uses: , or ; or tabular
+#sales_data <- read.csv("full path to your file", sep = ";", header = TRUE)
 
 
 ## --------------------------------------------------------------------------------------------------------------------------------------------------------------
-top10_sales
+#sales_data # Returns the entire data frame. NEVER DO IT. 
 
-## --------------------------------------------------------------------------------------------------------------------------------------------------------------
-date_most_sold
+head(sales_data, 3) # returns the first N rows (here, the first 3 rows)
+tail(sales_data, 3) # returns the last N rows (here, the last 3 rows)
 
-
-## --------------------------------------------------------------------------------------------------------------------------------------------------------------
-class(date_most_sold)
-
-
-## --------------------------------------------------------------------------------------------------------------------------------------------------------------
-sales_data <- data.frame(top10_sales, 
-                         top10_product_names, 
-                         private_label, 
-                         top10_brand, 
-                         date_most_sold, 
-                         private_label_logical)
-
-
-## --------------------------------------------------------------------------------------------------------------------------------------------------------------
-sales_data # Returns the entire data frame. NEVER DO IT AGAIN. 
-
-head(sales_data, 3) # returns the first X rows (here, the first 3 rows)
-tail(sales_data, 3) # returns the last X rows (here, the last 3 rows)
-
-names(sales_data) # returns the names of the variables in the data frame
+#names(sales_data) # returns the names of the variables in the data frame
 
 str(sales_data) # returns the structure of the data frame
 
 nrow(sales_data) # returns the number of rows 
 ncol(sales_data) # returns the number of columns 
-dim(sales_data) # returns the dimensions of a data frame
-
-ls(sales_data) # list all objects associated with an object
-
-ls() # list of all objects you created
 
 ## --------------------------------------------------------------------------------------------------------------------------------------------------------------
-sales_data$top10_sales
+sales_data$top10_sales # this is the way to "call" a specific column from a data table (or data frame). Read: top10_sales from sales_data
+# It makes the column "active" - using this notation, you can perform any operations with a specified column
 
 
 ## --------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -105,8 +80,13 @@ sales_data[ , 2:4] # all rows and columns 2,3,4
 sales_data[5:7, ] # rows 5,6,7 and all columns
 
 
+
 ## --------------------------------------------------------------------------------------------------------------------------------------------------------------
+### Operations with data
 library(tidyverse)
+
+# 1. Filtering (ROWS)
+# Filter by specific values
 filter(sales_data, private_label == "private label") # show only products that belong to private labels
 filter(sales_data, top10_sales > 100000) # show only products that sold more than 100,000 units  
 filter(sales_data, top10_product_names == 'Bio-Kaisersemmel') # returns all observations from product "Bio-Kaisersemmel"
@@ -117,6 +97,7 @@ sales_data %>% filter(private_label == "private label")
 
 
 ## --------------------------------------------------------------------------------------------------------------------------------------------------------------
+# 2. Arranging
 # Arrange by sales (descending: most - least) 
 arrange(sales_data, desc(top10_sales))
 
@@ -125,6 +106,7 @@ arrange(sales_data, top10_brand, desc(top10_sales))
 
 
 ## --------------------------------------------------------------------------------------------------------------------------------------------------------------
+# 3. Selecting (COLUMNS)
 # Keep only several columns. Again, if you need to use the result for analysis or show it in a presentation, you need to create the object. 
 select(sales_data, top10_product_names, top10_sales, private_label)
 
@@ -134,7 +116,6 @@ select(sales_data, private_label) %>% unique() # you can retrieve unique combina
 select(sales_data, top10_brand, private_label) %>% unique() # ... or unique combinations of different columns
 
 
-## --------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Another way to do the same - remove columns that you don't need
 select(sales_data, -date_most_sold, -private_label_logical)
 
@@ -149,6 +130,7 @@ head(sales_data)
 
 
 ## --------------------------------------------------------------------------------------------------------------------------------------------------------------
+# 4. Grouping and 5. Summarizing 
 # Get summary statistics by grouping observations by some characteristic: functions group_by() and summarize()
 sales_total <- sales_data %>% 
   group_by(top10_brand) %>%
@@ -157,6 +139,7 @@ head(sales_total)
 
 
 ## --------------------------------------------------------------------------------------------------------------------------------------------------------------
+# 6. Mutating
 sales_data_new <- mutate(sales_data, 
        sqrt_sales = sqrt(top10_sales),
        # "%m" extracts the month, format returns a character so we convert it to integer
@@ -172,15 +155,6 @@ sales_data <- dplyr::rename(sales_data, brand = top10_brand)
 head(sales_data)
 
 
-## Data import
-## --------------------------------------------------------------------------------------------------------------------------------------------------------------
-#load data from server
-readLines("https://short.wu.ac.at/ma22_musicdata", n = 3)
-test_data <- read.csv("https://short.wu.ac.at/ma22_musicdata", 
-                      sep = ";", 
-                      header = TRUE)
-head(test_data)
-
 ## --------------------------------------------------------------------------------------------------------------------------------------------------------------
 #load data from file on disk. You have to make sure that your working directory is set correctly, 
 # your R file is open from the working directory, and either your data file is located in the working directory 
@@ -190,6 +164,10 @@ head(test_data)
 
 
 ## --------------------------------------------------------------------------------------------------------------------------------------------------------------
-#export data
+# If you need to export your data (e.g., to send it to a colleague)
 write.csv(sales_data, "Sales_Data.csv", row.names = FALSE) # writes to a comma-separated value file
+
+
+## Let's practice!
+
 
